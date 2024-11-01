85cbb1280d04db8359beddf50cd7a61deae162e6
MINOUJSS <minoujss@gmail.com>
2024-11-01 12:48:44 +0100

    commit all line in boot() function in route service provider file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,27 +24,27 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        // RateLimiter::for('api', function (Request $request) {
+        //     return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        // });
 
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+        // $this->routes(function () {
+        //     Route::middleware('api')
+        //         ->prefix('api')
+        //         ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-            //my custom routes
-            Route::middleware('web')
-                ->group(base_path('routes/seller.php'));
-            Route::middleware('web')
-                ->group(base_path('routes/supplier.php'));
-            Route::middleware('web')
-                ->group(base_path('routes/shiper.php'));
-            Route::middleware('web')
-                ->group(base_path('routes/marketer.php'));
-        });
+        //     Route::middleware('web')
+        //         ->group(base_path('routes/web.php'));
+        //     //my custom routes
+        //     Route::middleware('web')
+        //         ->group(base_path('routes/seller.php'));
+        //     Route::middleware('web')
+        //         ->group(base_path('routes/supplier.php'));
+        //     Route::middleware('web')
+        //         ->group(base_path('routes/shiper.php'));
+        //     Route::middleware('web')
+        //         ->group(base_path('routes/marketer.php'));
+        // });
     }
     // RouteServiceProvider
 
